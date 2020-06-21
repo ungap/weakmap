902da0e101b07015b0e896b6bb7587efd9ea2a2d
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-06-21 12:41:03 +0200

    removed self dance in modules

--- cjs/index.js
+++ cjs/index.js
@@ -1,5 +1,5 @@
 /*! (c) Andrea Giammarchi - ISC */
-var self = this || /* istanbul ignore next */ {};
+var self = {};
 try { self.WeakMap = WeakMap; }
 catch (WeakMap) {
   // this could be better but 90% of the time
